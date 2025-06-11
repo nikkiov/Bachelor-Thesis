ee84f4d0c8b227dd91338f7a2fed6bbcc3b8199b
Nikki <n.overmars@student.utwente.nl>
2025-06-11 17:55:16 +0200

    Update MachineLearning_Depression_Sensitivity.R

--- MachineLearning_Depression_Sensitivity.R
+++ MachineLearning_Depression_Sensitivity.R
@@ -347,13 +347,13 @@ model_labels <- c(
   interact2_logistic = "Logistic, no covariates"
 )
 
-# Use iso calibration on the best performing model to measure performance with and without calibration
+# Use logistic regression calibration on the best performing model to measure performance with and without calibration
 # With thanks to: www.tidymodels.org/learn/models/calibration/
-iso_val <- cal_validate_isotonic_boot(resample_list_depression_sensitivity_read$interact3_logistic,
+log_val <- cal_validate_logistic(resample_list_depression_sensitivity_read$interact3_logistic,
                                       metrics = metric_set(roc_auc), 
                                       save_pred = TRUE, 
                                       times = 25)
-cell_cal <- cal_estimate_isotonic_boot(resample_list_depression_sensitivity_read$interact3_logistic)    # Calculate new probabilities
+cell_cal <- cal_estimate_logistic(resample_list_depression_sensitivity_read$interact3_logistic)    # Calculate new probabilities
 cal_fit <- wf_list_depression_sensitivity_read$interact3_logistic %>% 
   fit(data = train_data_depression_sensitivity_read)
 cell_test_pred <- augment(cal_fit, 
